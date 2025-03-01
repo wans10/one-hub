34625b14fa394598eba6ef993fc08abf44bd869c
wans10 <wans10@hotmail.com>
2025-03-01 21:24:49 +0800

    调整 className="application-container" 使 className="application-item" 在同一水平

--- web/src/views/Home/index.jsx
+++ web/src/views/Home/index.jsx
@@ -70,7 +70,7 @@ const Home = () => {
           <div className="application-item">
             <img src="/ai-icon.png" alt="API" />
             <h4>{t('API服务')}</h4>
-            <p>{t('支持多种模型，包括GPT-4o、Claude 3.5 Sonnet、Gemini 1.5 Pro、DALL-E等模型API调用。')}</p>
+            <p>{t('支持多种模型，包括ChatGPT、Claude、Grok、Gemini、DALL-E等大语言模型API调用。')}</p>
             <a href="https://www.llmhub.net/token" className="chat" target="_blank" rel="noreferrer">{t('开始部署')}</a>
           </div>
         </div>

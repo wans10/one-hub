7b26306507923041e319d02fa6285b28465d54eb
wans10 <wans10@hotmail.com>
2025-04-01 16:58:50 +0800

    调整页脚

--- web/src/ui-component/Footer.jsx
+++ web/src/ui-component/Footer.jsx
@@ -10,28 +10,12 @@ const Footer = () => {
   const siteInfo = useSelector((state) => state.siteInfo);
   const { t } = useTranslation();
 
+  const defaultFooterHtml = '© 2025 llmhub.com.cn 版权所有  <a href="https://beian.miit.gov.cn/" target="_blank">津ICP备2025029271号</a>  <img src="https://beian.mps.gov.cn/img/logo01.dd7ff50e.png" width="20"/> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=33010902004083" rel="noreferrer" target="_blank">浙公网安备33010902004083号</a>';
+
   return (
     <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '64px' }}>
       <Box sx={{ textAlign: 'center' }}>
-        {siteInfo.footer_html ? (
-          <div className="custom-footer" dangerouslySetInnerHTML={{ __html: siteInfo.footer_html }}></div>
-        ) : (
-          <>
-            <Link href="https://github.com/MartialBE/one-hub" target="_blank">
-              {siteInfo.system_name} {import.meta.env.VITE_APP_VERSION}{' '}
-            </Link>
-            {t('footer.developedBy')}{' '}
-            <Link href="https://github.com/MartialBE" target="_blank">
-              MartialBE
-            </Link>
-            ，{t('footer.basedOn')}{' '}
-            <Link href="https://github.com/songquanpeng" target="_blank">
-              JustSong
-            </Link>{' '}
-            LLM Hub，{t('footer.sourceCode')}
-            <Link href="https://opensource.org/licenses/mit-license.php"> {t('footer.license')}</Link>
-          </>
-        )}
+        <div className="custom-footer" dangerouslySetInnerHTML={{ __html: siteInfo.footer_html || defaultFooterHtml }}></div>
       </Box>
     </Container>
   );

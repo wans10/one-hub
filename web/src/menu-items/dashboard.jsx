1b45c764c5613514ca912d90db1e1f1959997512
wans10 <wans10@hotmail.com>
2025-05-20 22:14:14 +0800

    隐藏 /playground

--- web/src/menu-items/dashboard.jsx
+++ web/src/menu-items/dashboard.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 const icons = {
   IconDashboard: () => <Icon width={20} icon="solar:widget-2-bold-duotone" />,
   IconChartHistogram: () => <Icon width={20} icon="solar:chart-2-bold-duotone" />,
-  IconBallFootball: () => <Icon width={20} icon="solar:chat-round-line-bold-duotone" />,
+  // IconBallFootball: () => <Icon width={20} icon="solar:chat-round-line-bold-duotone" />,
   IconSystemInfo: () => <Icon width={20} icon="solar:code-scan-bold" />
 };
 
@@ -30,14 +30,14 @@ const dashboard = {
       breadcrumbs: false,
       isAdmin: true
     },
-    {
-      id: 'playground',
-      title: 'Playground',
-      type: 'item',
-      url: '/panel/playground',
-      icon: icons.IconBallFootball,
-      breadcrumbs: false
-    },
+    // {
+    //   id: 'playground',
+    //   title: 'Playground',
+    //   type: 'item',
+    //   url: '/panel/playground',
+    //   icon: icons.IconBallFootball,
+    //   breadcrumbs: false
+    // },
     {
       id: 'systemInfo',
       title: '系统信息',

1d74eb8c1599dfd6cc31e462ddd855672525227c
wans10 <wans10@hotmail.com>
2025-05-30 10:44:05 +0800

    更新 Api 与 Web 速率限制为 10800 与 1800

--- middleware/rate-limit.go
+++ middleware/rate-limit.go
@@ -19,10 +19,10 @@ var inMemoryRateLimiter common.InMemoryRateLimiter
 // All duration's unit is seconds
 // Shouldn't larger then RateLimitKeyExpirationDuration
 var (
-	GlobalApiRateLimitNum            = 180
+	GlobalApiRateLimitNum            = 10800
 	GlobalApiRateLimitDuration int64 = 3 * 60
 
-	GlobalWebRateLimitNum            = 100
+	GlobalWebRateLimitNum            = 1800
 	GlobalWebRateLimitDuration int64 = 3 * 60
 
 	UploadRateLimitNum            = 10

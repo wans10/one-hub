f6223bf3c4ca8fbbd771b4c8650fd58bdd0bb0fc
wans10 <wans10@hotmail.com>
2025-06-01 00:44:50 +0800

    更新页脚

--- web/src/ui-component/Footer.jsx
+++ web/src/ui-component/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
   const siteInfo = useSelector((state) => state.siteInfo);
   const { t } = useTranslation();
 
-  const defaultFooterHtml = '© 2025 llmhub.com.cn 版权所有  <a href="https://beian.miit.gov.cn/" target="_blank">津ICP备2025029271号</a>  <img src="https://beian.mps.gov.cn/img/logo01.dd7ff50e.png" width="20"/> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=33010902004083" rel="noreferrer" target="_blank">浙公网安备33010902004083号</a>';
+  const defaultFooterHtml = '© 2025 llmhub.com.cn 版权所有  <a href="https://beian.miit.gov.cn/" target="_blank">津ICP备2025029271号-1</a>  <img src="https://beian.mps.gov.cn/img/logo01.dd7ff50e.png" width="20"/> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=33010902004083" rel="noreferrer" target="_blank">浙公网安备33010902004083号</a>';
 
   return (
     <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '64px', borderRadius: 0 }}>
